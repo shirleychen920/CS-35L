5f509a3019ced858ff481bf8389040dba21708d1
shirleychen920 <chenhaojie94@gmail.com>
2015-11-30 18:02:03 -0800

    add dl

---
+++ Dynamic Linking/randlibhw.c
@@ -0,0 +1,23 @@
+#include "randlib.h"
+
+/* Initialize the hardware rand64 implementation.  */
+static void
+rand64_init (void)
+{
+}
+
+/* Return a random value, using hardware operations.  */
+extern unsigned long long
+rand64 (void)
+{
+  unsigned long long int x;
+  while (! _rdrand64_step (&x))
+    continue;
+  return x;
+}
+
+/* Finalize the hardware rand64 implementation.  */
+static void
+rand64_fini (void)
+{
+}
